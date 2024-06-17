56533b5253cbb61de3111444c5f0c2c1a6355bd6
Quinntyx <harrisonhenry916@gmail.com>
2024-06-16 21:12:17 -0500

    initial commit take 4

---
+++ makeshit/src/main.rs
@@ -0,0 +1,17 @@
+use std::path::PathBuf;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let archive_files: Vec<PathBuf> = args[1..args.len()]
+        .iter()
+        .map(|i| i.into())
+        .collect();
+    let archive_name = args.last()
+        .expect("Archive name should be specified");
+
+    libshit::Archive::new(&archive_files, true)
+        .serialize()
+        .compress()
+        .write(archive_name.into())
+        .expect("Archive should be writable");
+}
